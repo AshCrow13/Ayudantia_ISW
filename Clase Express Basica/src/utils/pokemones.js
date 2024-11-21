926dd2d30e619d198e63a08ec16eb8591f8ba5ed
AshCrow13 <omar.castro2001@alumnos.ubiobio.cl>
2024-11-21 19:01:53 -0300

    Clase express basica

---
+++ Clase Express Basica/src/utils/pokemones.js
@@ -0,0 +1,26 @@
+export let pokemones = [
+    {
+        id: 1,
+        nombre: 'Pikachu',
+        tipo: 'Electrico',
+        descripcion: 'Ratón eléctrico'
+    },
+    {
+        id: 2,
+        nombre: 'Charmander',
+        tipo: 'Fuego',
+        descripcion: 'Lagartija de fuego'
+    },
+    {
+        id: 3,
+        nombre: 'Squirtle',
+        tipo: 'Agua',
+        descripcion: 'Tortuga de agua'
+    },
+    {
+        id: 4,
+        nombre: 'Bulbasaur',
+        tipo: 'Planta',
+        descripcion: 'Dinosaurio de planta'
+    }
+];
