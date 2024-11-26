172af976e3ce2f11b0e07fbdccc81dc75ef8dd2b
AshCrow13 <omar.castro2001@alumnos.ubiobio.cl>
2024-11-26 03:08:35 -0300

    UPD Clase 5

---
+++ Clase 5/src/controllers/auth.controller.js
@@ -0,0 +1,26 @@
+"user strict";
+
+export async function login(req, res) {
+    try {
+        
+    } catch (error) {
+        
+    }
+}
+
+export async function register(req, res) {
+    try {
+        
+    } catch (error) {
+        
+    }
+}
+
+export async function logout(req, res) {
+    try {
+        
+    } catch (error) {
+        
+    }
+}
+
--- Clase 5/src/index.js
+++ Clase 5/src/index.js
@@ -1,15 +1,38 @@
 import express, { json } from 'express'; // Module
 import indexRoutes from './routes/index.routes.js';
-import { PORT, HOST } from './config/configEnv.js';
+import { PORT, HOST, cookieKey } from './config/configEnv.js';
 import { connectDB } from './config/configDb.js';
+import cookieParser from 'cookie-parser';
+import morgan from 'morgan';
+import session from 'express-session';
+import passport from 'passport';
+import { PassportJwtSetup } from './auth/passport-setup.js';
 
 async function setupServer() {
     try {
         const app = express(); // Instancia express y almacenar sus metodos en app
         
-        app.use(json());
+        app.use(json()); // Middleware para parsear el body de las peticiones a json
+        app.use(cookieParser()); // Middleware para parsear las cookies
+        app.use(morgan("dev")); // Middleware para loggear las peticiones
+        
+        app.use(session({ // Middleware para manejar las sesiones
+            secret: cookieKey, 
+            resave: false, 
+            saveUninitialized: false,
+            cookie: { 
+                secure: false,
+                httpOnly: true,
+                sameSite: "strict",
+            }
+        }));
+        
+        app.use(passport.initialize()); // Middleware para inicializar passport
+        app.use(passport.session()); // Middleware para manejar las sesiones de passport
 
-        app.use('/api', indexRoutes);
+        PassportJwtSetup(); // Configurar passport con la estrategia de JWT
+        
+        app.use('/api', indexRoutes); // Middleware para manejar las rutas de la API
 
         app.listen(PORT, () => {
             console.log(`Servidor corriendo en: http://${HOST}:${PORT}/api`);
